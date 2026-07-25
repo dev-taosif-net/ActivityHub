6b342a08ddde7c683dde64b1c1e006756079d70e
dev-taosif-net <dev.taosif.net@gmail.com>
2026-07-25 20:56:25 +0600

    Add NuGet support files for Infrastructure project
    - Created Infrastructure.csproj.nuget.g.props to define NuGet properties.
    - Added Infrastructure.csproj.nuget.g.targets for build targets.
    - Generated project.assets.json to manage project dependencies and assets.
    - Included project.nuget.cache for caching NuGet package information.

---
+++ Domain/Activity.cs
@@ -0,0 +1,24 @@
+namespace Domain;
+
+public class Activity
+{
+    public string Id { get; set; }= Guid.NewGuid().ToString();
+    public required string Title  { get; set; } 
+    public required string Description { get; set; }
+    public required string Category { get; set; }
+    public required DateTime Date { get; set; }
+    public bool IsCancelled { get; set; }
+    public required int MaxParticipants { get; set; }
+
+    // Location
+    public required string City { get; set; }
+    public required string Venue { get; set; }
+     public double Latitude { get; set; }
+    public double Longitude { get; set; }
+  
+
+     // navigation properties
+    // public ICollection<ActivityAttendee> Attendees { get; set; } = [];
+    // public ICollection<Comment> Comments { get; set; } = [];
+
+}
